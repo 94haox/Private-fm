6b175214b756249442e8eccd2fccff7a6cc4b64e
Duke.wu <497160282@qq.com>
2019-11-11 14:14:27 +0800

    [   FEATURE ] 添加解析请求

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,4 +1,7 @@
 'use strict';
+let ParserKit = require('rss-parser');
+let parser = new ParserKit();
+
 
 const Controller = require('egg').Controller;
 
@@ -7,6 +10,33 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+
+  async parserRss() {
+    let url = this.ctx.request.query['url']
+    let feed = await parser.parseURL(url);
+    this.success(feed)
+  }
+
+   success(detail) {
+     this.ctx.body = {
+       result: 1,
+       code: 0,
+       data: {
+         detail: detail
+       },
+       message: '操作成功'
+     }
+   }
+
+   error(msg, code = 1) {
+     this.ctx.body = {
+       result: 1,
+       data: {},
+       code: code,
+       message: msg
+     }
+   }
+
 }
 
 module.exports = HomeController;
